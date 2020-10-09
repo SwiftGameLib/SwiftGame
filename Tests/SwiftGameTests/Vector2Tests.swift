434b26b93f20ec35157748fe79f103484dd17abc
David Green <dgreen6@apple.com>
2020-10-09 11:36:04 -0400

    Added preliminary Vector2 unit tests.

---
+++ Tests/SwiftGameTests/Vector2Tests.swift
@@ -0,0 +1,48 @@
+//
+//  Vector2Tests.swift
+//  
+//
+//  Created by David Green on 10/9/20.
+//
+
+import XCTest
+@testable import SwiftGame
+
+final class Vector2Tests: XCTestCase {
+    func testMultiplication() {
+        let vector = Vector2(1, 2)
+
+        // Test 0.0 scale
+        XCTAssertEqual(Vector2.zero, 0 * vector)
+        XCTAssertEqual(Vector2.zero, vector * 0)
+
+        // Test 1.0 scale
+        XCTAssertEqual(vector, 1 * vector)
+        XCTAssertEqual(vector, vector * 1)
+
+        var scaledVec = Vector2(vector.x * 2, vector.y * 2)
+
+        // Test 2.0 scale
+        XCTAssertEqual(scaledVec, 2 * vector)
+        XCTAssertEqual(scaledVec, vector * 2)
+        XCTAssertEqual(vector * 2, scaledVec)
+
+        scaledVec = Vector2(vector.x * 0.999, vector.y * 0.999)
+
+        // Test 0.999 scale
+        XCTAssertEqual(scaledVec, 0.999 * vector)
+        XCTAssertEqual(scaledVec, vector * 0.999)
+        XCTAssertEqual(vector * 0.999, scaledVec)
+
+        let vector2 = Vector2(2, 2)
+
+        // Test vector multiplication
+        XCTAssertEqual(Vector2(vector.x * vector2.x, vector.y * vector2.y), vector * vector2)
+        XCTAssertEqual(vector2 * vector, Vector2(vector.x * vector2.x, vector.y * vector2.y))
+        XCTAssertEqual(vector * vector2, vector2 * vector)
+    }
+
+    static var allTests = [
+        ("testMultiplication", testMultiplication),
+    ]
+}
